5c505aaefb5a1ada2dfc2114f9606e2e1ce9c5f7
BananaKun <yanxun951224@gmail.com>
2021-06-28 16:00:25 +0100

    Solo Project

---
+++ M1-SoloProject.js
@@ -0,0 +1,540 @@
+/*
+    ASSIGNMENT RULES
+    - All the answers must be written in JavaScript
+    - The solution must be pushed to the repository and be available for the tutors by the end of the day
+    - You can ask for help, reach the Teaching Assistants if needed
+    - You can google / use StackOverflow BUT we suggest you to use just the material provided
+    - You can test your code in a separate file or de-commenting the single exercises in this one.
+      You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+    - Complete as many exercise that you can
+    - Publish them into your own GitHub account and upload repository link on Eduflow before 16.30 (Berlin Time) 
+*/
+
+//JS Basics
+
+/* Ex.A
+   Create a variable called "test" and assign a string to it.
+*/
+
+let test = "This is a string.";
+console.log("Ex A:", test);
+
+/* Ex.B
+    Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
+*/
+
+let sum = 0;
+for(let i = 10; i <= 20; i++){
+    sum += i;
+};
+console.log("\nEx B:", sum);
+
+/* Ex.C 
+    Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
+*/
+
+let random = Math.floor(Math.random() * 20);
+console.log("\nEx C:", random);
+
+/* Ex.D
+    Create a variable called "me" and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
+*/
+let me = {
+    name: "YanXun",
+    surname: "Chang",
+    age: 25
+}
+console.log("\nEx D:", me);
+
+/* Ex.E 
+    Programmatically remove the age property from the previously create object.
+*/
+delete me.age;
+console.log("\nEx E:", me);
+
+/* Ex.F 
+   Programmatically add to the object me an array called "skills", containing the programming languages you know right now.
+*/
+me.skills = ["Java", "Python", "JS"];
+console.log("\nEx F:", me);
+
+/* Ex.G 
+   Programmatically remove the last skill from the "skills" array inside the "me" object.
+*/
+delete me.skills.splice(-1,1)
+console.log("\nEx G:", me);
+
+// JS Functions
+/* Ex.1
+    Write a function called "dice"; it should randomize an integer number between 1 and 6.
+*/
+const dice = function(){
+    return Math.floor(Math.random() * 6) + 1;
+}
+console.log("\nEx 1:", dice());
+
+/* Ex.2 
+    Write a function called "whoIsBigger" which receives 2 numbers as parameters and returns the biggest one.
+*/
+const whoIsBigger = function(num1, num2){
+    return num1 > num2 ? num1 : num2;
+}
+console.log("\nEx 2:", whoIsBigger(2,8));
+
+/* Ex.3
+    Write a function called "splitMe" which receives a string as a parameter and returns an array with every word in that string.
+    Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
+*/
+const splitMe = function(string){
+    return string.split(" ");
+}
+console.log("\nEx 3:", splitMe("I love coding"));
+
+/* Ex.4
+    Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+*/
+const deleteOne = function(string, boolean){
+    let str;
+    if(boolean){
+        str = string.slice(1)
+    }else{
+        str = string.slice(0, -1)
+    }
+    return str;
+}
+console.log("\nEx 4a:", deleteOne("strive",true));
+console.log("Ex 4b:", deleteOne("school",false));
+
+/* Ex.5
+   Write a function called "onlyLetters" which receives a string as a parameter and returns it removing all the digits.
+   Ex.: onlyLetters("I have 4 dogs")  => returns "I have  dogs"
+*/
+const onlyLetters = function(string){
+    let patt1 = /[^0-9]/g;
+    let result = string.match(patt1);
+    return result.join("");
+}
+console.log("\nEx 5a:", onlyLetters("I have 4 dogs"));
+
+/* Ex.6 
+   Write a function called "isThisAnEmail" which receives a string as a parameter and returns true if the string is a valid email address.
+*/
+
+const isThisAnEmail = function(email){
+    const re = /\S+@\S+\.\S+/g;
+    let valid = true;
+
+    let result = re.test(email);
+    if (result) {
+        valid;
+    }
+    else {
+        valid = false;
+    }
+    return valid;
+}
+
+console.log("\nEx 6a:", isThisAnEmail("yanxun951224@gmail.com"));
+console.log("Ex 6b:", isThisAnEmail("yanxun951224gmail.com"));
+
+/* Ex.7
+   Write a function called "whatDayIsIt" that should return the current day of the week.
+*/
+const whatDayIsIt = function(){
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+}
+let date = whatDayIsIt();
+console.log("\nEx 8: Today is", date)
+
+
+/* Ex.8
+    Write a function called "rollTheDices" which receives a number as a parameter.
+    It should invoke the dice() function defined in Ex1 the specified amount of times,
+    and return an object containing a "sum" property holding the sum of all values extracted
+    and a "values" array containing the single values of the dicerolls themselves.
+    Example: RollTheDices(3) => returns {
+        sum: 10
+        values: [3, 3, 4]
+    }
+*/
+const rollTheDices = function(times){
+    let sum = 0;
+    values = [];
+    for(let i = 0; i < times; i++){
+        let num = Math.floor(Math.random() * 6) + 1;
+        values[i] = num;
+        sum += num;
+    }
+    let obj = {
+        sum,
+        values
+    }
+    return obj;
+}
+console.log("\nEx 8: RollTheDices(3) => returns", rollTheDices(3));
+
+/* Ex.9
+   Write a function called "howManyDays" which receives a date as a parameter and should return the number of days passed since that date.
+*/
+const howManyDays = function(d){
+    let currentDate = new Date();
+    let cDay = currentDate.getDate()
+    let dayPass = cDay - d;
+    return dayPass;
+}
+let days = howManyDays(12);
+console.log(`\nEx9: There are ${days} days since 12.`);
+
+/* Ex.10
+   Write a function called "isTodayMyBirthday" which should return true if today's your birthday, false otherwise.
+*/
+const isTodayMyBirthday = function(){
+    let birthday = true;
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    today = mm + '/' + dd
+    if(today == "12/24"){
+        birthday = true
+    }else{
+        birthday = false;
+    }
+    return birthday;
+}
+console.log("\nEx10: Is today my birthday?",isTodayMyBirthday());
+
+// JS Arrays // Objs
+// NOTE: movies array is defined at the end of this file!
+
+/* This movies array is used throughout the exercises. Please don't change it :)  */
+const movies = [
+    {
+      Title: "The Lord of the Rings: The Fellowship of the Ring",
+      Year: "2001",
+      imdbID: "tt0120737",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings: The Return of the King",
+      Year: "2003",
+      imdbID: "tt0167260",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings: The Two Towers",
+      Year: "2002",
+      imdbID: "tt0167261",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of War",
+      Year: "2005",
+      imdbID: "tt0399295",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lords of Dogtown",
+      Year: "2005",
+      imdbID: "tt0355702",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lord of the Rings",
+      Year: "1978",
+      imdbID: "tt0077869",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of the Flies",
+      Year: "1990",
+      imdbID: "tt0100054",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Lords of Salem",
+      Year: "2012",
+      imdbID: "tt1731697",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+      Year: "1984",
+      imdbID: "tt0087365",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+    },
+    {
+      Title: "Lord of the Flies",
+      Year: "1963",
+      imdbID: "tt0057261",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Avengers",
+      Year: "2012",
+      imdbID: "tt0848228",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Infinity War",
+      Year: "2018",
+      imdbID: "tt4154756",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Age of Ultron",
+      Year: "2015",
+      imdbID: "tt2395427",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+    },
+    {
+      Title: "Avengers: Endgame",
+      Year: "2019",
+      imdbID: "tt4154796",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+    },
+  ]
+
+/* Ex.11
+   Write a function called "deleteProp" which receives an object and a string as parameters, and returns the given object after deleting its property named as the given string.
+*/
+const deleteProp = function(arr, str){
+    let deleteMovie;
+    for(let i = 0; i< arr.length; i++){
+        if(arr[i].Title == str){
+            deleteMovie = arr[i];
+            arr.splice(i, 1);
+        }
+    }
+    return deleteMovie;
+}
+console.log("\nEx 11: This movie", deleteProp(movies, "Avengers: Endgame"), "has been deleted.")
+console.log(movies);
+
+/* Ex.12 
+    Write a function called "olderMovie" which finds the oldest movie in the array provided at the end of this file.
+*/
+const olderMovie = function(arr){
+    let oldestYear = arr[1].Year;
+    let oldestMovieName = " ";
+    for(let i = 0; i < arr.length; i++){
+        if( arr[i].Year < oldestYear){
+            oldestYear = arr[i].Year;
+            oldestMovieName = arr[i].Title;
+        }
+    }
+    return oldestMovieName;
+}
+
+console.log("\nEx 12: The oldest movie is \"", olderMovie(movies), "\".")
+
+/* Ex.13
+    Write a function called "countMovies" which returns the number of movies contained in the array provided at the end of this file.
+*/
+const countMovies = function(arr){
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        count++;
+    }
+    return count;
+}
+console.log("\nEx 13: The total movies inside the array are", countMovies(movies))
+
+/* Ex.14
+    Write a function called "onlyTheTitles" which creates an array with just the titles of the movies provided in the array at the end of the file.
+*/
+const onlyTheTitles = function(arr){
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        newArr[i] = arr[i].Title;
+    }
+    return newArr;
+}
+console.log("\nEx 14: New movie array with just title:", onlyTheTitles(movies))
+
+/* Ex.15
+   Write a function called "onlyInThisMillennium" which returns only the movies produced in this millennium.
+*/
+const onlyInThisMillennium = function(arr){
+    let thisMillennium = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].Year > 1999){
+            thisMillennium.push(arr[i].Title);
+        }
+    }
+    return thisMillennium;
+}
+console.log("\nEx 15: The movie produced in this millennium:", onlyInThisMillennium(movies))
+
+/* Ex.16 
+    Write a function called "getMovieById" which receives an id as a parameter and returns the movie with the given id.
+*/
+const getMovieById = function(imdId){
+    let movie = [];
+    for(let i = 0; i < movies.length; i++){
+        if(movies[i].imdbID === imdId){
+            movie = movies[i];
+        }
+    }
+    return movie;
+}
+console.log("\nEx 16: The movie with the imdID \"tt0077869\" is", getMovieById("tt0077869"))
+
+/* Ex.17
+    Write a function called "sumAllTheYears" which returns the sum of all the years in which the movies provided have been produced.
+*/
+const sumAllTheYears = function(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += parseInt(arr[i].Year);
+    }
+    return sum;
+}
+console.log("\nEx 17: The sum of all movie year are", sumAllTheYears(movies), "years.")
+
+/* Ex.18
+    Write a function called "searchByTitle" which receives a string as a parameter and returns all the movies which contain that string in the title.
+*/
+const searchByTitle = function(arr, str){
+    let ans = [];
+    let newArr = arr;
+    for(let i = 0; i < newArr.length; i++){
+        let haveStr = newArr[i].Title.includes(str);
+        if(haveStr == true){
+            ans.push(newArr[i].Title);
+        }
+    }
+    return ans;
+}
+console.log("\nEx 18: All the movie that contain \"the\" inside the title:", searchByTitle(movies, "the"))
+
+/* Ex.19
+    Write a function called "searchAndDivide" which receives a string as a parameter and returns an object;
+    this object should contain an array called "match", made by all the movies which contain the given string in the title,
+    and another array "unmatch" with all the remaining ones.
+*/
+const searchAndDivide = function(str){
+    let match = [];
+    let unmatch = [];
+    let newArr = movies;
+    for(let i = 0; i < newArr.length; i++){
+        let haveStr = newArr[i].Title.includes(str);
+        if(haveStr == true){
+            match.push(newArr[i].Title);
+        }else{
+            unmatch.push(newArr[i].Title);
+        }
+    }
+    let ans = {match, unmatch}
+    return ans;
+}
+console.log("\nEx 19: The sum of all movie year are", searchAndDivide("the"), "years.")
+
+/* Ex.20
+   Write a function called "removeIndex" which receives a number as a parameter and returns the movies array without the element in the given position.
+*/
+const removeIndex = function(num){
+    movies.splice(num,1);
+    return movies;
+}
+console.log("\nEx 20: The array after delete the index 0", removeIndex(0))
+
+// [EXTRAS] JS Advanced
+
+/* Ex.21
+  Create a function called "halfTree" which receives a number as a parameter and builds an "*" half tree with the given height.
+  Example:
+  halfTree(3)
+  *
+  **
+  ***
+*/
+const halfTree = function(num){
+    for(let i = 0; i < num; i++){
+        for(let j = 0; j <= i; j++){
+            process.stdout.write("*");
+        }
+        console.log(" ");
+    }
+}
+
+console.log("\nEx 21:")
+halfTree(3);
+
+/* Ex.22 
+  Create a function called "tree" which receives a number as a parameter and builds an "*" tree with the given height.
+  Example: 
+  tree(3)
+    *  
+   *** 
+  *****
+*/
+// const tree = function(num){
+//     for(let i = 0; i < num; i++){
+//         let n = num;
+//         for(let j = 0; j <= n-1; j++){
+//             process.stdout.write("");
+//             for(let k = i; k >= 1; k--){
+//                 process.stdout.write("*");
+//             }
+//         }
+//         console.log(" ");
+//     }
+// }
+// console.log("\nEx 21:")
+// tree(3);
+
+/* Ex.23
+  Create a function called "isItPrime" that receives a number as a parameter and returns true if the given number is a prime number.
+*/
+const isItPrime = function(number){
+    let isPrime = true;
+    let special = " ";
+    if (number === 1) {
+        return special = "1 is neither prime nor composite number."
+    }else if (number > 1) {
+        for (let i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+    }
+    return isPrime;
+}
+
+console.log("\nEx 23: Number 3 is", isItPrime(3))
